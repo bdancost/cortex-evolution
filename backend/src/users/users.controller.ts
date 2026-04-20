533df5c50fb82a61cbdb366307d0cb548d3e86be
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-04-19 21:29:05 -0300

    feat(users): implement user creation with prisma and dto

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -8,5 +9,11 @@ export class UsersController {
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @Post()
+  create(@Body() body: CreateUserDto) {
+    console.log('BODY:', body);
+    return this.usersService.create(body);
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateUserDto) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
   findAll() {
-    return ['user1', 'user2'];
+    return this.prisma.user.findMany();
   }
 }
