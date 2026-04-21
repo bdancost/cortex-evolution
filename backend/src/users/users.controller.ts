8a7ffdfdad25bd6f00d7d75835b34d4c6074e515
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-04-20 21:29:12 -0300

    feat(auth): protect routes using jwt guard

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,19 +1,30 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Prisma } from '@prisma/client';
+
+type UserSafe = Prisma.UserGetPayload<{
+  select: {
+    id: true;
+    name: true;
+    email: true;
+    createdAt: true;
+  };
+}>;
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {
+  findAll(): Promise<UserSafe[]> {
     return this.usersService.findAll();
   }
 
   @Post()
-  create(@Body() body: CreateUserDto) {
-    console.log('BODY:', body);
+  create(@Body() body: CreateUserDto): Promise<UserSafe> {
     return this.usersService.create(body);
   }
 }
